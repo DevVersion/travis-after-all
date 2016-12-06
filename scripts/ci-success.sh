cef045af124bbd778e32696607f736246ff32184
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-12-06 17:10:23 +0100

    Initial commit

---
+++ scripts/ci-success.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Go to the project root directory
+cd $(dirname $0)/../
+
+echo "Testing other modes after success"
+
+node src/index.js
+
---
+++ scripts/ci-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Waiting 2min"
+
+sleep 7200
+
+echo "Done with Waiting"
+
+exit 0
