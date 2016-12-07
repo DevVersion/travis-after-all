4f1348c1baedd2b2954fd568ecaefc05ec01d418
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-12-07 18:26:42 +0100

    Add observable to wait for builds

--- src/index.js
+++ src/index.js
@@ -6,22 +6,45 @@ if (!process.env['TRAVIS_BUILD_ID']) {
 }
 
 const request = require('request-promise');
+const Rx = require('rxjs');
 
 const API_ENDPOINT = 'https://api.travis-ci.org';
-const BUILD_ID = process.env['TRAVIS_BUILD_ID'];
+const CHECK_INTERVAL = 10 * 1000;
 
-sendRequest('builds/' + BUILD_ID)
-  .then(result => result['matrix'])
-  .then(matrixes => console.log(matrixes));
+const buildId = process.env['TRAVIS_BUILD_ID'];
+const jobNumber = process.env['TRAVIS_JOB_NUMBER'];
+
+// The leader job will always wait for the others jobs to finish.
+if (!jobNumber.endsWith('.1')) {
+  return;
+}
+
+Rx.Observable
+  .interval(CHECK_INTERVAL)
+  .switchMap(() => getOtherJobs().every(job => !!job['finished_at']))
+  .filter(x => !!x)
+  .take(1)
+  .subscribe(x => console.log("Finished with all builds"));
+
+/**
+ * Retrieves data about the other Travis CI jobs running in a matrix.
+ * @returns {Observable.<Object>}
+ */
+function getOtherJobs() {
+  return sendRequest('builds/' + buildId)
+    .map(result => result['matrix']);
+}
 
 /**
  * Sends a request to the Travis CI API and parses its json.
  * @param requestUrl Rest URL to access
- * @returns {Promise.<Object>} Parsed JSON Object
+ * @returns {Observable.<Object>} Parsed JSON Object
  */
 function sendRequest(requestUrl) {
-  return request({
-    url: `${API_ENDPOINT}/${requestUrl}`,
-    json: true
-  });
+  return Rx.Observable.fromPromise(
+    request({
+      url: `${API_ENDPOINT}/${requestUrl}`,
+      json: true
+    })
+  );
 }
