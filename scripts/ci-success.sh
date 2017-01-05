2dddb2744f9cf43b0e8671ef753fec79ffd687da
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-01-05 14:51:53 +0100

    Update checks to validate M2

--- scripts/ci-success.sh
+++ scripts/ci-success.sh
@@ -4,7 +4,7 @@
 cd $(dirname $0)/../
 
 # Check if all other modes passed successfully.
-if node src/index.js; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && node src/index.js; then
   echo "Everything passed"
 else
   echo "Something failed"
